f5e9e50a18e7320a8a7adfab2432b7c3f6df7a04
Ahmadreza Esfandiari <a.esfandiari.didex@gmail.com>
2023-06-15 17:00:57 +0330

    creat class member

--- cw 20/books/models.py
+++ cw 20/books/models.py
@@ -7,3 +7,21 @@ class Book(models.Model):
     publication_date = models.DateField()
     ISBN = models.CharField(max_length=100)
     availability_status = models.BooleanField()
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    biography = models.TextField()
+    email = models.EmailField()
+
+class Member(models.Model):
+    class StatusChoices(models.TextChoices):
+        ACTIVE = ('ac', 'active')
+        DEACTIVATE = ('dc', 'deactivate')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    contact_number = models.IntegerField()
+    address = models.CharField(max_length=200)
+    status = models.CharField(max_length=2, choices=StatusChoices.choices,
+    default=StatusChoices.DEACTIVATE)
+
+
