e3efdbbe520d7aca5afee25e965b111ae35ad7f1
Ahmadreza Esfandiari <a.esfandiari.didex@gmail.com>
2023-06-16 15:36:59 +0330

    connecting views & templates

--- cw 20-3/TextToHtmlConverter/converter_app/views.py
+++ cw 20-3/TextToHtmlConverter/converter_app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils.html import escape
 
 def converter(request):
-    return render(request, 'converter_app/converter.html')
+    if request.method == 'POST':
+        text = request.POST.get('text', '')
+        converted_html = escape(text).replace('\n', '<br>')
+        return render(request, 'converter_app/result.html', {'converted_html': converted_html})
+    else:
+        return render(request, 'converter_app/converter.html')
