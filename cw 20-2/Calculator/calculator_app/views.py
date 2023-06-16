8284a2e234aaf7d94d9087db35268b1c84b9718b
Ahmadreza Esfandiari <a.esfandiari.didex@gmail.com>
2023-06-16 14:44:30 +0330

    Adding connection to views & templates

--- cw 20-2/Calculator/calculator_app/views.py
+++ cw 20-2/Calculator/calculator_app/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def home(request):
-    return render(request, 'calculator_app/home.html')
+    return render(request, 'calculator_app/calculator.html')
 
 def add(request, num1, num2):
     result = num1 + num2
-    return render(request, 'calculator_app/result.html', {'result': result})
+    return render(request, 'calculator_app/add.html', {'num1': num1, 'num2': num2, 'result': result})
 
 def subtract(request, num1, num2):
     result = num1 - num2
-    return render(request, 'calculator_app/result.html', {'result': result})
+    return render(request, 'calculator_app/subtract.html', {'num1': num1, 'num2': num2, 'result': result})
 
 def multiply(request, num1, num2):
     result = num1 * num2
-    return render(request, 'calculator_app/result.html', {'result': result})
+    return render(request, 'calculator_app/multiply.html', {'num1': num1, 'num2': num2, 'result': result})
 
 def divide(request, num1, num2):
     result = num1 / num2
-    return render(request, 'calculator_app/result.html', {'result': result})
+    return render(request, 'calculator_app/divide.html', {'num1': num1, 'num2': num2, 'result': result})
